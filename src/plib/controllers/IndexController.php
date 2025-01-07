2b74573f0f49e88a3806097e71e4a1c28ff2ea68
Dmitriy Kaminskiy <dmitriy.kaminskiy@webpros.com>
2025-01-07 11:15:57 +0200

    FIX PPP-66673 update copyright

--- src/plib/controllers/IndexController.php
+++ src/plib/controllers/IndexController.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2024. WebPros International GmbH. All rights reserved.
+// Copyright 1999-2025. WebPros International GmbH. All rights reserved.
 
 class IndexController extends pm_Controller_Action
 {
